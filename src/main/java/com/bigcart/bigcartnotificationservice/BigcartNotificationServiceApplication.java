0115269983874fde7a15878d0707f6e33a39f0ef
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-23 18:03:43 -0400

    First commit

---
+++ src/main/java/com/bigcart/bigcartnotificationservice/BigcartNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigcart.bigcartnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcartNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigcartNotificationServiceApplication.class, args);
+    }
+
+}
